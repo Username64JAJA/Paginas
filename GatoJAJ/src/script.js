ec6fea243f06af09c3b67580b9371941938d1d0b
Username64JAJA <montigamer123567@gmail.com>
2024-08-28 19:55:04 -0300

    Delete GatoJAJ/src directory

--- GatoJAJ/src/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-    var audio = document.getElementById('background-audio');
-    if (audio) {
-        audio.volume = 0.5; // Ajusta el volumen si es necesario
-        audio.play().catch(function(error) {
-            console.error('Error al intentar reproducir el audio:', error);
-        });
-    }
-});
